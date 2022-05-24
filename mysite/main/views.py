03163885f55019556c648e67de26fa367c7587df
tehcbing <16199244+tehcbing@users.noreply.github.com>
2022-05-24 13:35:47 +0800

    Tech with Tim Django Tutorial Part 4

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -5,10 +5,8 @@ from .models import ToDoList, Item
 # Create your views here (a view is kind of like a webpage)
 
 def index(response, id):
-	# get todolist based on id
 	ls = ToDoList.objects.get(id=id)
+	return render(response, "main/list.html", {"ls":ls})
 
-	#get items in todolist
-	item = ls.item_set.get(id=1)
-
-	return HttpResponse("<h1>%s</h1> %s" %(ls.name, item.text)) 
+def home(response):
+	return render(response, "main/home.html", {"name":"test"})
